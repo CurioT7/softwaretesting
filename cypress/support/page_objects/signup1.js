d1c472b041f523236133bd533bdb136ab0d0639a
maramtarek377 <117278528+maramtarek377@users.noreply.github.com>
2024-04-01 23:15:30 +0200

    Add files via upload

---
+++ cypress/support/page_objects/signup1.js
@@ -0,0 +1,83 @@
+export class SignUpPage{
+   get goButton(){
+    return cy.get('div.align-items-center > .link-offcanvas > .dropdown-toggle').click()
+   }
+    get loginButton(){  
+       return cy.get('.dropdown-menu > .d-flex').click()
+    }
+    get signUpButton(){
+        return cy.contains('Button', 'Signup')
+    }
+    get signUpWithGoogle(){
+        return cy.get('[data-testid="google-signup"]')
+    }
+    get signUpWithApple(){
+        return cy.get('[data-testid="Apple-signup"]')
+    }
+    get xButton(){
+        return cy.get('Button', 'close')
+    }
+    get emailFieldErrorMessage(){
+        return cy.get('[data-testid="email"]')
+    }
+    get emailField(){
+        return cy.get('[data-testid="email"]')
+    }
+    get emailEnter(){
+        return cy.get('.signup-info').click()
+    }
+    get continue1(){
+      return   cy.get('.pb-4').click()
+    }
+    get continueButton(){
+        return cy.get('.pb-4 > .w-100')
+        .click()
+    }
+    get usernameField(){
+        return cy.get('[data-testid="username"]')
+    }
+    get usernameFieldMessage(){
+        return cy.get('[data-testid="username"]')
+    }
+    get usernameFieldErrorMessageValid(){
+        return cy.get(':nth-child(3) > .p-2')
+    }
+    get usernameFieldEnter(){
+        return cy.get('.signup-header').click()
+    }
+    get passwordField(){
+        return cy.get('[data-testid="password"]')
+    }
+   get passwordFieldMessage(){
+    return cy.get(':nth-child(3) > .p-2')
+   }
+   get passwordFieldEnter(){
+    return cy.get('.signup-header').click()
+}
+    get continue4Button(){
+        return cy.get('.pb-4 > .w-100').click({force:true})
+    }
+    get continue2Button(){
+        return cy.get('.pb-4').click()
+    }
+    get selectgenderButton(){
+        return cy.get('.modal-body > .d-flex > :nth-child(2)').click()
+    }
+    get selectgenderButton2(){
+        return cy.get('.d-flex > :nth-child(3)').click()
+    }
+    get continue3Button(){
+        return cy.get('.pb-4 > .w-100').click()
+    }
+    get backButton(){
+        return cy.get('.signup-back-button').click()
+    }
+    get skipButton(){
+        return cy.get('.skip-button').click()
+    }
+    get intrestButton(){
+        return cy.get(':nth-child(3) > :nth-child(9)').select('MLB')
+    }
+}
+
+export default new SignUpPage();
