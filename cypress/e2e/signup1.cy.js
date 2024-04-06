929c4c651ce9b3da4868eda7e462b7b52bb4af55
maramtarek377 <117278528+maramtarek377@users.noreply.github.com>
2024-04-06 18:09:42 +0200

    Update signup1.cy.js
    updated signup sprint1.2

--- cypress/e2e/signup1.cy.js
+++ cypress/e2e/signup1.cy.js
@@ -1,13 +1,12 @@
 
-import  {SignUpPage } from "../support/page_objects/signup1"
+import  {SignUpPage } from "../../cypress/support/page_objects/signup1"
 // import{data} from"../../fixtures/data.json"
 Cypress.on('uncaught:exception', (err, runnable) => {
     // returning false here prevents Cypress from
     // failing the test
     return false
   })
-describe('signup', () => {
-//     
+describe('signup', () => {   
     it('signup', () => {
         const signUpPage = new SignUpPage(); 
         cy.visit('http://localhost:5173/')
@@ -15,7 +14,8 @@ describe('signup', () => {
         cy.wait(3000)
         signUpPage.loginButton
         cy.wait(3000)
-        signUpPage.emailField.should('be.visible').type("ali1213@gmail.com")
+        signUpPage.signUpWithGoogle
+        signUpPage.emailField.should('be.visible').type("sm1234@gmail.com")
         signUpPage.continueButton
         cy.wait(4000)
         signUpPage.usernameField.clear().type("du")
@@ -23,9 +23,9 @@ describe('signup', () => {
        
          signUpPage.passwordField.type("12")
          signUpPage.continue2Button
-        signUpPage.usernameField.clear().type("dummy134gdh")
+        signUpPage.usernameField.clear().type("dummy134gdgd")
          cy.wait(4000)
-         signUpPage.passwordField.clear().type("34132sf34")
+         signUpPage.passwordField.clear().type("a123b4567")
         cy.wait(2000)
         signUpPage.continue4Button
         cy.wait(4000)
@@ -36,12 +36,38 @@ describe('signup', () => {
         cy.wait(3000)
         signUpPage.skipButton
         cy.wait(3000)
-    //     signUpPage.intrestButton
-    //    cy.wait(3000)
-    //    signUpPage.continue3Button
-
-       
+        signUpPage.intrestButton
+        cy.wait(3000)
+        signUpPage.continue3Button
     })
+
+    it('signup with google', ()=>{
+        const signUpPage = new SignUpPage(); 
+        cy.visit('http://localhost:5173/')
+        signUpPage.goButton
+        cy.wait(3000)
+        signUpPage.loginButton
+        cy.wait(3000)
+        signUpPage.signUpWithGoogle
+
+    })
+    // old existing account
+    // signUpPage.goButton
+    //     cy.wait(1000)
+    //     signUpPage.loginButton
+    //     cy.wait(2000)
+    //     signUpPage.emailField.should('be.visible').type("ali1213@gmail.com")
+    //     signUpPage.continueButton
+    //     signUpPage.usernameField.clear().type("dummy134gdh")
+    //      cy.wait(2000)
+    //      signUpPage.passwordField.clear().type("34132sf34")
+    //     cy.wait(2000)
+    //     signUpPage.continue4Button
+    //     signUpPage.skipButton
+    //     signUpPage.intrestButton
+    //     cy.wait(1000)
+    //     signUpPage.continue2Button
+    //     cy.wait(1000)
 
    
 })
