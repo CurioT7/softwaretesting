4bfdeab72aaf04162ff49211b790330360de540f
Sama Elshorbagy <101284922+Samaelshorbagy@users.noreply.github.com>
2024-05-11 20:43:03 +0300

    Update login.py

--- appium/f1-sprint1/login.py
+++ appium/f1-sprint1/login.py
@@ -1,78 +1,59 @@
-import pytest 
+import pytest
 from appium import webdriver
 from appium.webdriver.common.appiumby import AppiumBy
 from appium.options.common import AppiumOptions
-from appium.webdriver.common.touch_action  import TouchAction
-from time import sleep 
+# from appium.webdriver.common.touch_action  import TouchAction
+from time import sleep
 from typing import Any, Dict
 
-desired_caps: Dict[str,Any] = {
+# desired caps
+desired_caps: Dict[str, Any] = {
 
-  "platformName": "Android",
-  "appium:platformVe": "14",
-  "appium:deviceName": "emulator-5554",
-  "appium:automationName": "Uiautomator2",
-  "appium:app": "C:\\Users\\maram\\Desktop\\Cross-Platform-main\\build\\app\\outputs\\apk\\debug\\app-debug.apk"
-
+    "platformName": "Android",
+    "appium:platformVe": "14",
+    "appium:deviceName": "emulator-5554",
+    "appium:automationName": "Uiautomator2",
+    # "appium:app": "C:\\Users\\samae\\OneDrive\\Desktop\\sprint2_2\\cross\\Cross-Platform-main\\build\\app\\outputs\\apk\\debug\\app-debug.apk",
+    "adbExecTimeout": 60000
 }
 
-url ='http://ocalhost:4723'
-driver= webdriver.remote(url, options=AppiumOptions().load_capabilities(desired_caps))
+url = 'http://localhost:4723'
+driver = webdriver.Remote(url, options=AppiumOptions().load_capabilities(desired_caps))
+driver.implicitly_wait(10)
+# sleep(20)
 
-#lockers
-logButton_Xpath_login="//android.widget.Button"
-loginbutton_Xpath_login="/android.widget.Button[@content-desc='Login']"
-passwordfield_Xpath_login=""
-login1button_Xpath_login=""
-usernamefield_Xpath_login=""
+# lockers
+logbutton = "//android.widget.Button"
+loginbutton = "//android.widget.Button[@content-desc=Login]"
+loginUsernameFeild = "//android.widget.FrameLayout[@resource-id=android:id/content]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.widget.EditText[1]"
+loginPassFeild = "//android.widget.FrameLayout[@resource-id=android:id/content]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.widget.EditText[2]"
+login = ""
+
+contWithGoogle = "//android.widget.ImageView[@content-desc=Continue with Google]"
+googleEmail = ""
+
 
 def login_username():
-    driver.find_element(by=AppiumBy.xpath,value=logButton_Xpath_login="//android.widget.Button").click()
-    usernamefield= driver.find_element(by=AppiumBy.xpath,value="").click()
+    driver.find_element(by=AppiumBy.XPATH, value=loginbutton).click()
+    usernamefield = driver.find_element(by=AppiumBy.XPATH, value=loginUsernameFeild)
     usernamefield.click()
     sleep(2)
-    usernamefield.send_keys('')
+    usernamefield.send_keys('samamostafa')
     sleep(2)
-    passwordfield = driver.find_element(by=AppiumBy.xpath,value="").click()
+    passwordfield = driver.find_element(by=AppiumBy.XPATH, value=loginPassFeild)
     passwordfield.click()
-    passwordfield.send_keys('')
+    passwordfield.send_keys('sama1212')
     sleep(2)
     driver.implicity_wait(3)
-    login1button= driver.find_element(by=AppiumBy.xpath,value="").click()
+    login1button = driver.find_element(by=AppiumBy.XPATH, value=login)
     login1button.click()
+    sleep(5)
 
 
-driver.quit()
 
-def login_wrongusername():
-    driver.find_element(by=AppiumBy.xpath,value="").click()
-    usernamefield= driver.find_element(by=AppiumBy.xpath,value="").click()
-    usernamefield.click()
+def login_google():
+    driver.find_element(AppiumBy.XPATH, value=contWithGoogle).click()
     sleep(2)
-    usernamefield.send_keys('')
+    googleemail = driver.find_element(by=AppiumBy.XPATH, value=googleEmail)
+    googleemail.click()
     sleep(2)
-    passwordfield = driver.find_element(by=AppiumBy.xpath,value="").click()
-    passwordfield.click()
-    passwordfield.send_keys('')
-    sleep(2)
-    driver.implicity_wait(3)
-    login1button= driver.find_element(by=AppiumBy.xpath,value="").click()
-    login1button.click()
-   
-driver.quit()
-
-def login_wrongpassword():
-    usernamefield= driver.find_element(by=AppiumBy.xpath,value="").click()
-    usernamefield.click()
-    sleep(2)
-    usernamefield.send_keys('')
-    sleep(2)
-    passwordfield = driver.find_element(by=AppiumBy.xpath,value="").click()
-    passwordfield.click()
-    passwordfield.send_keys('')
-    sleep(2)
-    driver.implicity_wait(3)
-    login1button= driver.find_element(by=AppiumBy.xpath,value="").click()
-    login1button.click()
-   
-driver.quit()
