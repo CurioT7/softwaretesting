5870f06659f1a21d68f5f0ae617e130c2d31c53d
Sama Elshorbagy <101284922+Samaelshorbagy@users.noreply.github.com>
2024-05-11 20:43:55 +0300

    Update signup.py

--- appium/f1-sprint1/signup.py
+++ appium/f1-sprint1/signup.py
@@ -2,110 +2,109 @@ import pytest
 from appium import webdriver
 from appium.webdriver.common.appiumby import AppiumBy
 from appium.options.common import AppiumOptions
-from appium.webdriver.common.touch_action  import TouchAction
+# from appium.webdriver.common.touch_action  import TouchAction
 from time import sleep 
 from typing import Any, Dict
 
+# desired caps
 desired_caps: Dict[str,Any] = {
-    "platformName": "Android",
-    "appium:deviceName":"Pixel2XL",
-    "appium:automationName":"" ,
-    
+  "platformName": "Android",
+  "appium:platformVe": "14",
+  "appium:deviceName": "emulator-5554",
+  "appium:automationName": "Uiautomator2",
+  "appium:app": "C:\\Users\\samae\\OneDrive\\Desktop\\sprint2_2\\cross\\Cross-Platform-main\\build\\app\\outputs\\apk\\debug\\app-debug.apk",
+  "adbExecTimeout": 60000
 }
 
-url =''
-driver= webdriver.remote(url, options=AppiumOptions().load_capabilities(desired_caps))
+
+url ='http://localhost:4723'
+driver= webdriver.Remote(url, options=AppiumOptions().load_capabilities(desired_caps))
 
 #lockers
-# profilrbutton_Xpath_signup=""
-# loginsignupbutton_Xpath_signup=""
-# loginbutton_Xpath_signup=""
-signupbutton_Xpath_signup=""
-emailfield_Xpath_signup=""
-passwordfield_Xpath_signup=""
-continue1button_Xpath_signup=""
-usernamefield_Xpath_signup=""
-continue2button_Xpath_signup=""
-aboutyoubutton_Xpath_signup=""
-continue3button_Xpath_signup=""
-intrestsubutton_Xpath_signup=""
-continue4button_Xpath_signup=""
-selectavatarbutton_Xpath_signup=""
-continue5button_Xpath_signup=""
+
+signupButton="//android.widget.Button[@content-desc=Sign Up]"
+signupEmailFeild="//android.widget.FrameLayout[@resource-id=android:id/content]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.widget.EditText[1]"
+signupPassFeild="//android.widget.FrameLayout[@resource-id=android:id/content]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.widget.EditText[2]"
+continueButton="//android.widget.Button[@content-desc=Continue]"
+backButton="//android.widget.Button[@content-desc=Back]"
+aboutYou=""
+interestButton=""
+
+
+
 
 def signup_email():
-    driver.find_element(by=AppiumBy.xpath,value="").click()
-    emailfield = driver.find_element(by=AppiumBy.xpath,value="").click()
+    driver.find_element(by=AppiumBy.XPATH,value="").click()
+    emailfield = driver.find_element(by=AppiumBy.XPATH,value=signupEmailFeild)
     emailfield.click()
+    emailfield.send_keys('samaelshorbajy@gmail.com')
     sleep(2)
-    emailfield.send_keys('')
-    sleep(2)
-    passwordfield = driver.find_element(by=AppiumBy.xpath,value="").click()
+    passwordfield = driver.find_element(by=AppiumBy.XPATH,value="")
     passwordfield.click()
-    passwordfield.send_keys('')
+    passwordfield.send_keys('eng19sam!')
     sleep(2)
     driver.implicity_wait(3)
-    continue1button= driver.find_element(by=AppiumBy.xpath,value="").click()
+    continue1button= driver.find_element(by=AppiumBy.XPATH,value=continueButton)
     continue1button.click()
     sleep(2)
-    usernamefield = driver.find_element(by=AppiumBy.xpath,value="").click()
+    usernamefield = driver.find_element(by=AppiumBy.XPATH,value="")
     usernamefield.click()
     sleep(2)
     usernamefield.send_keys('')
     sleep(2)
-    continue2button= driver.find_element(by=AppiumBy.xpath,value="").click()
+    continue2button= driver.find_element(by=AppiumBy.XPATH,value="")
     continue2button.click()
     sleep(2)
-    aboutyoubutton= driver.find_element(by=AppiumBy.xpath,value="").click()
+    aboutyoubutton= driver.find_element(by=AppiumBy.XPATH,value="")
     aboutyoubutton.click()
     sleep(2)
-    continue3button= driver.find_element(by=AppiumBy.xpath,value="").click()
+    continue3button= driver.find_element(by=AppiumBy.XPATH,value="")
     continue3button.click()
     sleep(2)
-    intrestsubutton =driver.find_element(by=AppiumBy.xpath,value="").click()
+    intrestsubutton =driver.find_element(by=AppiumBy.XPATH,value="")
     intrestsubutton.click()
     sleep(2)
-    continue4button= driver.find_element(by=AppiumBy.xpath,value="").click()
+    continue4button= driver.find_element(by=AppiumBy.XPATH,value="")
     continue4button.click()
-    selectavatarbutton=driver.find_element(by=AppiumBy.xpath,value="").click()
+    selectavatarbutton=driver.find_element(by=AppiumBy.XPATH,value="")
     selectavatarbutton.click()
     sleep(2)
-    continue5button= driver.find_element(by=AppiumBy.xpath,value="").click()
+    continue5button= driver.find_element(by=AppiumBy.XPATH,value="")
     continue5button.click()
 
 driver.quit()
 
 def signup_wrongemail():
-    driver.find_element(by=AppiumBy.xpath,value="").click()
-    emailfield = driver.find_element(by=AppiumBy.xpath,value="").click()
+    driver.find_element(by=AppiumBy.XPATH,value="").click()
+    emailfield = driver.find_element(by=AppiumBy.XPATH,value="")
     emailfield.click()
     sleep(2)
     emailfield.send_keys('')
     sleep(2)
-    passwordfield = driver.find_element(by=AppiumBy.xpath,value="").click()
+    passwordfield = driver.find_element(by=AppiumBy.XPATH,value="")
     passwordfield.click()
     passwordfield.send_keys('')
     sleep(2)
     driver.implicity_wait(3)
-    continue1button= driver.find_element(by=AppiumBy.xpath,value="").click()
+    continue1button= driver.find_element(by=AppiumBy.XPATH,value="")
     continue1button.click()
     sleep(2)
    
 driver.quit()
 
 def signup_wrongpassword():
-    driver.find_element(by=AppiumBy.xpath,value="").click()
-    emailfield = driver.find_element(by=AppiumBy.xpath,value="").click()
+    driver.find_element(by=AppiumBy.XPATH,value="").click()
+    emailfield = driver.find_element(by=AppiumBy.XPATH,value="")
     emailfield.click()
     sleep(2)
     emailfield.send_keys('')
     sleep(2)
-    passwordfield = driver.find_element(by=AppiumBy.xpath,value="").click()
+    passwordfield = driver.find_element(by=AppiumBy.XPATH,value="")
     passwordfield.click()
     passwordfield.send_keys('')
     sleep(2)
     driver.implicity_wait(3)
-    continue1button= driver.find_element(by=AppiumBy.xpath,value="").click()
+    continue1button= driver.find_element(by=AppiumBy.XPATH,value="")
     continue1button.click()
     sleep(2)
    
