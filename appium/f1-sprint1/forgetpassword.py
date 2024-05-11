b10055c3aec123bfa83cc17d3a2701cd2ad34cf3
Sama Elshorbagy <101284922+Samaelshorbagy@users.noreply.github.com>
2024-05-11 20:44:43 +0300

    Update forgetpassword.py

--- appium/f1-sprint1/forgetpassword.py
+++ appium/f1-sprint1/forgetpassword.py
@@ -2,7 +2,7 @@ import pytest
 from appium import webdriver
 from appium.webdriver.common.appiumby import AppiumBy
 from appium.options.common import AppiumOptions
-from appium.webdriver.common.touch_action  import TouchAction
+# from appium.webdriver.common.touch_action  import TouchAction
 from time import sleep 
 from typing import Any, Dict
 
@@ -20,134 +20,145 @@ url ='http://ocalhost:4723'
 driver= webdriver.remote(url, options=AppiumOptions().load_capabilities(desired_caps))
 
 #lockers
-viewmenuButton_Xpath_forgetpassword="//android.widget.Button"
-login_signupbutton_Xpath_forgetpassword="/android.widget.Button[@content-desc='Login']"
-loginButton_Xpath_forgetpassword=""
-login1button_Xpath_forgetpassword=""
-usernamefield_Xpath_forgetpassword=""
-forgetpasswordButton_Xpath_forgetpassword=""
-emailfield_Xpath_forgetpassword=""
-resetpassword_Xpath_forgetpassword=""
-openemailappButton_Xpath_forgetpassword=""
-gmailButton_Xpath_forgetpassword=""
-openButton_Xpath_forgetpassword=""
-resetyourpasswordButton_Xpath_forgetpassword=""
-defaultappButton_Xpath_forgetpassword=""
-chooseyourusernameButton_Xpath_forgetpassword=""
-reset1password_Xpath_forgetpassword=""
-newpassword_Xpath_forgetpassword=""
-confirmpassword_Xpath_forgetpassword=""
-continueButton_Xpath_forgetpassword=""
-resetpassword_Xpath_forgetpassword=""
+loginButton="//android.widget.Button[@content-desc=Login]"
+forgotPass="//android.widget.Button[@content-desc=Forgot Password?]"
+userName="//android.widget.FrameLayout[@resource-id=android:id/content]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.widget.EditText[1]"
+userEmail="//android.widget.FrameLayout[@resource-id=android:id/content]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.widget.EditText[2]"
+helpButton="//android.widget.Button[@content-desc=Help]"
+resetPassButton="//android.widget.Button[@content-desc=Reset Password]"
 
-def login_username():
-    driver.find_element(by=AppiumBy.xpath,value=viewmenuButton_Xpath_forgetpassword="//android.widget.Button").click()
-    viewmenuButton= driver.find_element(by=AppiumBy.xpath,value="").click()
-    viewmenuButton.click()
-    sleep(2)
-   # usernamefield.send_keys('')
-    sleep(2)
-    login_signupbutton= driver.find_element(by=AppiumBy.xpath,value="").click()
-    login_signupbutton.click()
-   
-    sleep(2)
-    driver.implicity_wait(3)
-    loginButton= driver.find_element(by=AppiumBy.xpath,value="").click()
-    loginButton.click()
-
-    sleep(2)
-    driver.implicity_wait(3)
-    login1button= driver.find_element(by=AppiumBy.xpath,value="").click()
-    login1button.click()
+def reset_password():
     
+    driver.find_element(by=AppiumBy.XPATH, value=loginButton)
+    forgetPassword=driver.find_element(by=AppiumBy.XPATH,value=forgotPass).click()
+    forgetPassword.click()
     sleep(2)
-    driver.implicity_wait(3)
-    usernamefield= driver.find_element(by=AppiumBy.xpath,value="").click()
+    usernamefield= driver.find_element(by=AppiumBy.XPATH,value=userName).click()
     usernamefield.click()
-    usernamefield.send_keys('')
-    
+    usernamefield.send_keys('samamostafa')
     sleep(2)
-    driver.implicity_wait(3)
-    forgetpasswordButton= driver.find_element(by=AppiumBy.xpath,value="").click()
-    forgetpasswordButton.click()
-    
+    useremailfield=driver.find_element(by=AppiumBy.XPATH,value=userEmail).click()
+    useremailfield.click()
+    useremailfield.send_keys('samaelshorbajy@gmail.com')
     sleep(2)
+
     driver.implicity_wait(3)
-    emailfield= driver.find_element(by=AppiumBy.xpath,value="").click()
-    emailfield.click()
-    emailfield.send_keys('')
-    
-    sleep(2)
-    driver.implicity_wait(3)
-    resetpassword= driver.find_element(by=AppiumBy.xpath,value="").click()
-    resetpassword.click()
-    
-    sleep(2)
-    driver.implicity_wait(3)
-    openemailappButton= driver.find_element(by=AppiumBy.xpath,value="").click()
-    openemailappButton.click()
-    
-    sleep(2)
-    driver.implicity_wait(3)
-    gmailButton= driver.find_element(by=AppiumBy.xpath,value="").click()
-    gmailButton.click()
-    
-    sleep(2)
-    driver.implicity_wait(3)
-    openButton= driver.find_element(by=AppiumBy.xpath,value="").click()
-    openButton.click() 
-    
-    sleep(2)
-    driver.implicity_wait(3)
-    resetyourpasswordButton= driver.find_element(by=AppiumBy.xpath,value="").click()
-    resetyourpasswordButton.click()
-    
-    sleep(2)
-    driver.implicity_wait(3)
-    defaultappButton= driver.find_element(by=AppiumBy.xpath,value="").click()
-    defaultappButton.click()
-    
-    sleep(2)
-    driver.implicity_wait(3)
-    openemailappButton= driver.find_element(by=AppiumBy.xpath,value="").click()
-    openemailappButton.click()
-    
-    sleep(2)
-    driver.implicity_wait(3)
-    chooseyourusernameButton= driver.find_element(by=AppiumBy.xpath,value="").click()
-    chooseyourusernameButton.click()
-    
-    sleep(2)
-    driver.implicity_wait(3)
-    reset1password= driver.find_element(by=AppiumBy.xpath,value="").click()
-    reset1password.click()
-    
-    sleep(2)
-    driver.implicity_wait(3)
-    chooseyourusernameButton= driver.find_element(by=AppiumBy.xpath,value="").click()
-    chooseyourusernameButton.click()
-     
-    sleep(2)
-    driver.implicity_wait(3)
-    newpassword= driver.find_element(by=AppiumBy.xpath,value="").click()
-    newpassword.click()
-    newpassword.send_keys('')
-    
-    sleep(2)
-    driver.implicity_wait(3)
-    confirmpassword= driver.find_element(by=AppiumBy.xpath,value="").click()
-    confirmpassword.click()
-    confirmpassword.send_keys('')
-    
-    sleep(2)
-    driver.implicity_wait(3)
-    continueButton= driver.find_element(by=AppiumBy.xpath,value="").click()
-    continueButton.click()
-    
-    sleep(2)
-    driver.implicity_wait(3)
-    resetpassword= driver.find_element(by=AppiumBy.xpath,value="").click()
+    resetpassword= driver.find_element(by=AppiumBy.XPATH,value=resetPassButton).click()
     resetpassword.click()
 
 driver.quit()
+
+
+
+# def login_username():
+#     driver.find_element(by=AppiumBy.XPATH,value=loginButton).click()
+
+#     viewmenuButton= driver.find_element(by=AppiumBy.XPATH,value="").click()
+#     viewmenuButton.click()
+#     sleep(2)
+#    # usernamefield.send_keys('')
+#     sleep(2)
+#     login_signupbutton= driver.find_element(by=AppiumBy.XPATH,value="").click()
+#     login_signupbutton.click()
+   
+#     sleep(2)
+#     driver.implicity_wait(3)
+#     loginButton= driver.find_element(by=AppiumBy.XPATH,value="").click()
+#     loginButton.click()
+
+#     sleep(2)
+#     driver.implicity_wait(3)
+#     login1button= driver.find_element(by=AppiumBy.XPATH,value="").click()
+#     login1button.click()
+    
+#     sleep(2)
+#     driver.implicity_wait(3)
+#     usernamefield= driver.find_element(by=AppiumBy.XPATH,value="").click()
+#     usernamefield.click()
+#     usernamefield.send_keys('')
+    
+#     sleep(2)
+#     driver.implicity_wait(3)
+#     forgetpasswordButton= driver.find_element(by=AppiumBy.XPATH,value="").click()
+#     forgetpasswordButton.click()
+    
+#     sleep(2)
+#     driver.implicity_wait(3)
+#     emailfield= driver.find_element(by=AppiumBy.XPATH,value="").click()
+#     emailfield.click()
+#     emailfield.send_keys('')
+    
+#     sleep(2)
+#     driver.implicity_wait(3)
+#     resetpassword= driver.find_element(by=AppiumBy.XPATH,value="").click()
+#     resetpassword.click()
+    
+#     sleep(2)
+#     driver.implicity_wait(3)
+#     openemailappButton= driver.find_element(by=AppiumBy.XPATH,value="").click()
+#     openemailappButton.click()
+    
+#     sleep(2)
+#     driver.implicity_wait(3)
+#     gmailButton= driver.find_element(by=AppiumBy.XPATH,value="").click()
+#     gmailButton.click()
+    
+#     sleep(2)
+#     driver.implicity_wait(3)
+#     openButton= driver.find_element(by=AppiumBy.XPATH,value="").click()
+#     openButton.click() 
+    
+#     sleep(2)
+#     driver.implicity_wait(3)
+#     resetyourpasswordButton= driver.find_element(by=AppiumBy.XPATH,value="").click()
+#     resetyourpasswordButton.click()
+    
+#     sleep(2)
+#     driver.implicity_wait(3)
+#     defaultappButton= driver.find_element(by=AppiumBy.XPATH,value="").click()
+#     defaultappButton.click()
+    
+#     sleep(2)
+#     driver.implicity_wait(3)
+#     openemailappButton= driver.find_element(by=AppiumBy.XPATH,value="").click()
+#     openemailappButton.click()
+    
+#     sleep(2)
+#     driver.implicity_wait(3)
+#     chooseyourusernameButton= driver.find_element(by=AppiumBy.XPATH,value="").click()
+#     chooseyourusernameButton.click()
+    
+#     sleep(2)
+#     driver.implicity_wait(3)
+#     reset1password= driver.find_element(by=AppiumBy.XPATH,value="").click()
+#     reset1password.click()
+    
+#     sleep(2)
+#     driver.implicity_wait(3)
+#     chooseyourusernameButton= driver.find_element(by=AppiumBy.XPATH,value="").click()
+#     chooseyourusernameButton.click()
+     
+#     sleep(2)
+#     driver.implicity_wait(3)
+#     newpassword= driver.find_element(by=AppiumBy.XPATH,value="").click()
+#     newpassword.click()
+#     newpassword.send_keys('')
+    
+#     sleep(2)
+#     driver.implicity_wait(3)
+#     confirmpassword= driver.find_element(by=AppiumBy.XPATH,value="").click()
+#     confirmpassword.click()
+#     confirmpassword.send_keys('')
+    
+#     sleep(2)
+#     driver.implicity_wait(3)
+#     continueButton= driver.find_element(by=AppiumBy.XPATH,value="").click()
+#     continueButton.click()
+    
+#     sleep(2)
+#     driver.implicity_wait(3)
+#     resetpassword= driver.find_element(by=AppiumBy.XPATH,value="").click()
+#     resetpassword.click()
+
+# driver.quit()
 
